f4861979b45c86da670392a7c5d34fc69e004d86
Bhavya Raj Joshi <bhavyarajjoshiatwork@gmail.com>
2024-03-19 20:44:58 +0530

    debugging 002

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -37,11 +37,14 @@ class ConfigurationManager:
             base_model_path = Path(config.base_model_path),
             updated_base_model_path = Path(config.updated_base_model_path),
             params_image_size = self.params.IMAGE_SIZE,
+
             params_learning_rate = self.params.LEARNING_RATE,
             params_include_top = self.params.INCLUDE_TOP,
             params_weights = self.params.WEIGHTS,
             params_classes = self.params.CLASSES
         )
+
+        return prepare_base_model_config
 
     def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
 
